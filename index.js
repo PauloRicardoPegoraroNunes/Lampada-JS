9a6b5c0e4ac9b84714f60cd127244c9fa096c5ec
Paulo Ricardo Pegoraro Nunes <pauloricardopegoraronunes2@gmail.com>
2019-04-20 10:33:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function botao(){
+    document.getElementById('image').src = "lampada-on.gif";
+    
+
+}
+
+function botao2(){
+    document.getElementById('image').src = "lampada-off.gif";
+   
+
+}
